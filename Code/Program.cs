53c3ccc3cdf47a33f19cfeb05782c9dae49634f0
Pek3p5 <97115175+Pek3p5@users.noreply.github.com>
2023-10-18 14:04:56 +0300

    Add files via upload

---
+++ Code/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine($"Введите количество элементов массива");
+int lengthArr = int.Parse(Console.ReadLine());
+int ifarr =0;
+string[] arr = new string[lengthArr];
+string[] farr = new string[lengthArr];
+int number = 3;
+System.Console.WriteLine("Введите массив");
+for (int i = 0; i < lengthArr; i++)
+{    
+    System.Console.WriteLine($"Введите {i+1} член массива");
+    arr[i] = Console.ReadLine();
+}
+for (int i = 0; i < lengthArr; i++)
+{
+    if(arr[i].Length <= number)
+    {
+        farr[ifarr] = arr[i];
+        ifarr++;
+    }
+}
+var str = string.Join(" ", farr);
+System.Console.WriteLine(str);
